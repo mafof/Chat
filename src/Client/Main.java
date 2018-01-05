0136e98a339b05c36cf08bf5b79cee24bab38d66
ratatyq <pash.bickov@mail.ru>
2018-01-05 17:44:51 +0300

    upd gitignore and refactoring Main.java

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -11,63 +11,65 @@ import Client.NetworkSend;
 import Utility.FilesHadler;
 
 /**
- * порты =>
- * 6688 Для принятие клиентам сообщений 
- * 5588 Для отправки на сервер сообщений
- * @author admin
- *
+ * РџРѕСЂС‚С‹ =>
+ * 6688 - Client part
+ * 5588 - Server part
+ * @author mafof
  */
 
 public class Main {
+	// Declaring variables for next initialization =>
 	public static Gui gui;
 	public static ClientNetworkThreadReceive net;
-	public static boolean isSetting = false;
-	public static String ip = null;
-	public static String nickname = null;
-	public static FilesHadler fh = new FilesHadler("settings.txt");
 	public static NetworkSend netSend;
-	public static String chatData = "";
+	public static FilesHadler fh = new FilesHadler("settings.txt");
+
+	// Date =>
 	public static Date date = new Date();
 	public static SimpleDateFormat format;
 
+	// Settings =>
+	public static String chatData = ""; // Data
+	public static boolean isSetting = false;
+	public static String ip = null;
+	public static String nickname = null;
+
 	public static void main(String[] args) {
+		// settings date time =>
 		date = new Date();
-		format = new SimpleDateFormat("hh:mm:ss:SS");
-		
-		gui = new Gui(null);
-		net = new ClientNetworkThreadReceive();
+		format = new SimpleDateFormat("hh:mm:ss");
+
+		// init classes =>
+		gui = new Gui(); // create gui
+		net = new ClientNetworkThreadReceive(); // create Network chart
 		checkSettings();
-		
-		/*
-		if(checkMessageCommand("!pm 192.168.1.4 Hello World", "!pm ") != null) {
-			System.out.println(true);
-		}
-		*/
 	}
-	
-	public static String getData() {
+
+	/**
+	 * Get date in need format
+	 * @return current date with need format
+	 */
+	public static String getDate() {
 		date = new Date();
 		return format.format(date);
 	}
-	
-	
+
 	/**
-	 * Проверка пришедшего сообщения
-	 * Формат такого сообщения следующий =>
-	 * Тип;значение;доп.параметры
+	 * Checked type message
+	 * @param _msg - user message from server
 	 */
 	public static void checkReceiveMessage(String _msg) {
 		String [] msg = _msg.split(";");
 		if(msg[0].equals("msg")) {
 			updateChatWindow(msg[1]);
 		} else if(msg[0].equals("privMsg")) {
-			String _tempMsg = "[Приватное сообщение от " + msg[1] + "] " + msg[2];
+			String _tempMsg = "[Private message from " + msg[1] + "] " + msg[2];
 			updateChatWindow(_tempMsg);
 		}
 	}
 	
 	/**
-	 * Чтение ini файла с настройками
+	 * Check data through ini files
 	 */
 	public static void checkSettings() {
 		if(fh.isFiles()) {
@@ -81,7 +83,7 @@ public class Main {
 	}
 	
 	/**
-	 * Заполнены ли переменные в настройках
+	 * Check data variables properties
 	 */
 	public static void checkSettingsVariable() {
 		if(ip != null && nickname != null && !ip.isEmpty() && !nickname.isEmpty()) {
@@ -92,17 +94,18 @@ public class Main {
 			isSetting = false;
 		}
 	}
-	
+
 	/**
-	 * Обновление вывода чата
+	 * Adding message with server
+	 * @param str - message
 	 */
 	private static void updateChatWindow(String str) {
-		chatData += "[" + getData() + "] " + str + "\n";
+		chatData += "[" + getDate() + "] " + str + "\n";
 		gui.chatWindow.setText(chatData);
 	}
 	
 	/**
-	 * RegEx проверка введенного сообщения на наличие команды
+	 * Check command on whitespace
 	 */
 	public static String checkMessageCommand(String msg, String reg) {
 		Pattern p = Pattern.compile(reg);
